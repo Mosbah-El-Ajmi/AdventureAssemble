eabde22ab4f0a333f7c3a52097a9771d13b16a0e
Jerome Verkyndt <j.verkyndt@students.ephec.be>
2024-03-19 22:18:27 +0100

    ajout dossier frontend

---
+++ frontend/src/ListeUtilisateurs.js
@@ -0,0 +1,33 @@
+import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const App = () => {
+    const [missions, setMissions] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:3001/utilisateurs')
+            .then(response => {
+                setMissions(response.data);
+            })
+            .catch(error => {
+                console.error('Error fetching missions:', error);
+            });
+    }, []);
+
+    return (
+        <div className="App">
+            <h1>Liste des missions</h1>
+            <div className="missions-list">
+                {missions.map(mission => (
+                    <div key={mission.id_mission} className="mission">
+                        <h2>{mission.nom_mission}</h2>
+                        <p>{mission.description_mission}</p>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default App;
