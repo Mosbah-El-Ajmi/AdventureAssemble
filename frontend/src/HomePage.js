e4879559006bfac6b5fda43231df863e9326b1ca
Mosbah-El-Ajmi <72753059+Mosbah-El-Ajmi@users.noreply.github.com>
2024-04-01 13:05:05 +0200

    Add files via upload

--- frontend/src/HomePage.js
+++ frontend/src/HomePage.js
@@ -1,29 +1,34 @@
-import './App.css';
+import React from 'react';
+import { useNavigate } from 'react-router-dom'; // Importe useNavigate
+import './HomePage.css';
 
 function HomePage() {
+  let navigate = useNavigate(); // Utilise l'hook useNavigate pour la navigation
+
+  const handleButtonClick = () => {
+    navigate("/login-or-sign-in"); // Navigue vers la route /login-or-sign-in lorsque le bouton est cliqué
+  };
+
   return (
-    <div className="App">
-      <footer className="App-header">
-        <div className="main">
-          <div className="coteacote">
-          <form>
-            <input type="submit" value="Créer un compte"/>
-            <input type="submit" value="Connexion"/>
-          </form>
-          </div>
-          <p>
-            Rejoindre une partie
-          </p>
-          <form>
-            <label>
-              <input type="text" name="code" placeholder="code"/>
-            </label>
-            <input type="submit" value="→"/>
-          </form>
+    <div className="app">
+      <div className="split-layout">
+        <div className="image-section">
+          {/* L'image sera ajoutée via CSS */}
         </div>
-      </footer>
+        <div className="text-section">
+          <h1>Une aventure IRL à votre goût</h1>
+          <p>Accomplissez des missions de folie entre potes, n'importe quand, n'importe où!</p>
+          <p>Proposez vos idées les plus funs, défiez vos potes, et montez dans le classement pour devenir le Champion de la team. Qu'on soit partants pour explorer, créer, ou juste se marrer, Aventure Assemble vous promet des moments inoubliables et des défis à relever ensemble.</p>
+          <p>Alors, cap ou pas cap ? Rejoignez-nous et préparez-vous à des aventures IRL inoubliables!</p>
+          <button className="cta" onClick={handleButtonClick}>Créer une mission dès maintenant</button>
+        </div>
+      </div>
     </div>
   );
 }
 
 export default HomePage;
+
+
+
+
