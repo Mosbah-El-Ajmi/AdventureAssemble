e89bfcf2268b0068fccef05f768a43dada73376a
RouRou6 <HE126060@students.ephec.be>
2024-04-04 20:47:57 +0200

    es màj sur page menu

--- frontend/src/components/Menu.js
+++ frontend/src/components/Menu.js
@@ -1,23 +1,21 @@
 import React, { useEffect } from "react";
 import "../css/Menu.css";
-import { Link, useLocation } from 'react-router-dom';
+import { Link, useLocation } from "react-router-dom";
 import {
-  GiMatterStates, 
-  GiProgression, 
-  GiGoldBar, 
-  GiPodiumWinner, 
+  GiMatterStates,
+  GiProgression,
+  GiGoldBar,
+  GiPodiumWinner,
   GiSiren,
   GiBigGear,
-  GiHamburgerMenu
-} from 'react-icons/gi';
-
+  GiHamburgerMenu,
+} from "react-icons/gi";
 
 function Menu() {
   const location = useLocation();
 
   useEffect(() => {
-    const  mainMenuLi = document
-    .querySelectorAll("#mainMenu li, .lastMenu li");
+    const mainMenuLi = document.querySelectorAll("#mainMenu li, .lastMenu li");
 
     function changeActive() {
       mainMenuLi.forEach((n) => n.classList.remove("active"));
@@ -26,27 +24,67 @@ function Menu() {
 
     mainMenuLi.forEach((n) => n.addEventListener("click", changeActive));
 
-    if (location.pathname === '/dashboard') {
-      const podiumWinnerLink = document.querySelector("#mainMenu li:first-child");
+    if (location.pathname === "/dashboard") {
+      const podiumWinnerLink = document.querySelector(
+        "#mainMenu li:first-child"
+      );
       podiumWinnerLink.classList.add("active");
     }
-
   }, [location.pathname]);
 
   return (
     <menu>
-
       <ul id="mainMenu">
-        <li><Link to="/dashboard"><GiPodiumWinner /></Link></li>
-        <li><Link to="/progression"><GiProgression /></Link></li>
-        <li><Link to="/rewards"><GiGoldBar /></Link></li>
-        <li><Link to="/history"><GiMatterStates /></Link></li>
-        <li><Link to="/notification"><GiSiren /></Link></li>
+        <li>
+          <Link to="/dashboard">
+            <span>
+              <GiPodiumWinner />
+            </span>
+          </Link>
+        </li>
+        <li>
+          <Link to="/progression">
+            <span>
+              <GiProgression />
+            </span>
+          </Link>
+        </li>
+        <li>
+          <Link to="/rewards">
+            <span>
+              <GiGoldBar />
+            </span>
+          </Link>
+        </li>
+        <li>
+          <Link to="/history">
+            <span>
+              <GiMatterStates />
+            </span>
+          </Link>
+        </li>
+        <li>
+          <Link to="/notification">
+            <span>
+              <GiSiren />
+            </span>
+          </Link>
+        </li>
       </ul>
 
       <ul className="lastMenu">
-        <li><Link to="/parameter"><GiBigGear /></Link></li>
-        <li><Link to="/menu"><GiHamburgerMenu /></Link></li>
+        <li>
+          <Link to="/parameter">
+            <span>
+              <GiBigGear />
+            </span>
+          </Link>
+        </li>
+        <li>
+          <Link to="/menu">
+            <GiHamburgerMenu />
+          </Link>
+        </li>
       </ul>
     </menu>
   );
