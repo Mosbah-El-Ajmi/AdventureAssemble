6d1ccd76547bb3d3946ea49ef80ebed0acb0b517
Jerome Verkyndt <j.verkyndt@students.ephec.be>
2024-05-25 14:23:36 +0200

    misse a jour header

--- frontend/src/components/Menu.js
+++ frontend/src/components/Menu.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "../css/Menu.css";
 import { Link, useLocation } from "react-router-dom";
+import { FaListCheck } from "react-icons/fa6";
+import { GrValidate } from "react-icons/gr";
 import {
   GiTimeBomb,
   GiAbstract038,
@@ -29,6 +31,18 @@ function Menu() {
           <Link to="/classement">
             <GiHistogram />
             <span className="linkName">Classement</span>
+          </Link>
+        </li>
+        <li className={activeItem === "/liste-missions" ? "active" : ""}>
+          <Link to="/liste-missions">
+            <FaListCheck />
+            <span className="linkName">Suivi des misssions</span>
+          </Link>
+        </li>
+        <li className={activeItem === "/validation" ? "active" : ""}>
+          <Link to="/validation">
+            <GrValidate />
+            <span className="linkName">Valider missions</span>
           </Link>
         </li>
         <li className={activeItem === "/rewards" ? "active" : ""}>
