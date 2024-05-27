b50b7936393a926bf2dabbed297b39f173dd2c3b
RouRou6 <HE126060@students.ephec.be>
2024-05-27 16:34:36 +0200

    1: Reglage responsive pour Menu, Dashboard, Graphique, Historique. 2: Page Recompense.

---
+++ frontend/src/components/Rewards.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Menu from "./Menu";
+import Recompense from "./Recompense";
+
+function Rewards() {
+  return (
+    <div className="dashboard">
+      <Menu />
+      <Recompense />
+    </div>
+  );
+}
+
+export default Rewards;
